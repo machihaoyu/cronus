7a143bfd23942869b6a8d8307c053fa4ceeec8f6
xiedejie <xiedejie@fangjinsuo.com>
2017-07-13 09:59:35 +0800

    Saas接口

---
+++ src/main/java/com/fjs/cronus/api/SaasController.java
@@ -0,0 +1,17 @@
+package com.fjs.cronus.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/7/12 0012.
+ */
+@RestController
+@RequestMapping("/saas/api/v1")
+public class SaasController {
+
+
+
+
+
+}
