863c450e9f77db3b98b31caad217a11e0f90530d
xiedejie <xiedejie@fangjinsuo.com>
2017-07-20 16:37:08 +0800

    saas首页接口

--- src/main/java/com/fjs/cronus/api/SaasController.java
+++ src/main/java/com/fjs/cronus/api/SaasController.java
@@ -4,8 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.fjs.cronus.dto.crm.ResponseData;
 import com.fjs.cronus.dto.saas.MineAchievementDTO;
+import com.fjs.cronus.dto.saas.SaasApiDTO;
 import com.fjs.cronus.dto.saas.SaasIndexDTO;
-import com.fjs.cronus.dto.uc.BaseUcDTO;
 import com.fjs.cronus.exception.ExceptionValidate;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
@@ -35,17 +35,17 @@ public class SaasController {
     @ApiOperation(value="Saas首页数据", notes="Saas首页数据接口API")
     @RequestMapping(value = "/api/v1/getSaasIndexData", method = RequestMethod.GET)
     @ResponseBody
-    public BaseUcDTO getSaasIndexData() {
+    public SaasApiDTO getSaasIndexData() {
         try {
             String url = saleUrl + "crmCountData?key=" + saasKey;
             String res = restTemplate.getForObject(url, String.class);
             ResponseData data = JSONObject.parseObject(res, ResponseData.class);
             ExceptionValidate.validateResponse(data);
             SaasIndexDTO saasIndexDTO = JSON.parseObject(data.getRetData(), SaasIndexDTO.class);
-            return new BaseUcDTO(0, null, saasIndexDTO);
+            return new SaasApiDTO(0, null, saasIndexDTO);
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
-            return new BaseUcDTO(8000, e.getMessage(), null);
+            return new SaasApiDTO(8000, e.getMessage(), null);
         }
     }
 
@@ -59,17 +59,17 @@ public class SaasController {
     })
     @RequestMapping(value = "/api/v1/getCrmAchievement", method = RequestMethod.GET)
     @ResponseBody
-    public BaseUcDTO getCrmAchievement(@RequestParam String userId) {
+    public SaasApiDTO getCrmAchievement(@RequestParam String userId) {
         try {
             String url = saleUrl + "getMyData?key=" + saasKey + "&user_id=" + userId;
             String res = restTemplate.getForObject(url, String.class);
             ResponseData data = JSONObject.parseObject(res, ResponseData.class);
             ExceptionValidate.validateResponse(data);
             MineAchievementDTO mineAchievementDTO = JSON.parseObject(data.getRetData(), MineAchievementDTO.class);
-            return new BaseUcDTO(0, null, mineAchievementDTO);
+            return new SaasApiDTO(0, null, mineAchievementDTO);
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
-            return new BaseUcDTO(8001, e.getMessage(), null);
+            return new SaasApiDTO(8001, e.getMessage(), null);
         }
     }
 
--- src/main/java/com/fjs/cronus/dto/saas/SaasApiDTO.java
+++ src/main/java/com/fjs/cronus/dto/saas/SaasApiDTO.java
@@ -3,19 +3,41 @@ package com.fjs.cronus.dto.saas;
 /**
  * Created by Administrator on 2017/7/14 0014.
  */
-public class SaasApiDTO {
+public class SaasApiDTO<T> {
 
-    private Object data;
+    private T data;
+    private String message;
+    private int result;
 
-    public SaasApiDTO(Object data) {
+    public SaasApiDTO(){}
+
+    public SaasApiDTO(int result, String message, T data) {
+        this.result = result;
+        this.message = message;
         this.data = data;
     }
 
-    public Object getData() {
+    public T getData() {
         return data;
     }
 
-    public void setData(Object data) {
+    public void setData(T data) {
         this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
     }
 }
