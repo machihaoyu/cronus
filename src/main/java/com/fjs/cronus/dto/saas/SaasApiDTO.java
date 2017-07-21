7a36038468a7be765a63c96693d8d526612d835a
xiedejie <xiedejie@fangjinsuo.com>
2017-07-21 14:25:05 +0800

    saas接口

--- src/main/java/com/fjs/cronus/dto/saas/SaasApiDTO.java
+++ src/main/java/com/fjs/cronus/dto/saas/SaasApiDTO.java
@@ -1,5 +1,12 @@
 package com.fjs.cronus.dto.saas;
 
+import com.fjs.cronus.dto.uc.FegionExceptionDTO;
+import com.fjs.cronus.dto.uc.FegionResponseErrorDTO;
+import com.fjs.cronus.util.StringAsciiUtil;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Map;
+
 /**
  * Created by Administrator on 2017/7/14 0014.
  */
@@ -13,8 +20,27 @@ public class SaasApiDTO<T> {
 
     public SaasApiDTO(int result, String message, T data) {
         this.result = result;
-        this.message = message;
-        this.data = data;
+        if (StringUtils.isEmpty(message)) {
+            this.message = "请求成功!";
+        } else {
+            Map<String, Object> map = StringAsciiUtil.fegionException(message);
+            if (null != map && map.size() > 0) {
+                if (map.containsKey("fegionExceptionDTO")) {
+                    FegionExceptionDTO fegionExceptionDTO = (FegionExceptionDTO)map.get("fegionExceptionDTO");
+                    this.message = fegionExceptionDTO.getError();
+                } else {
+                    FegionResponseErrorDTO responseError = (FegionResponseErrorDTO)map.get("responseError");
+                    this.message = responseError.getMessage();
+                }
+            } else {
+                this.message = message;
+            }
+        }
+        if (null == data) {
+            this.data = (T)"";
+        } else {
+            this.data = data;
+        }
     }
 
     public T getData() {
