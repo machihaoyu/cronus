83ea93925db70e6cc800b0806c12ecde62babad0
tb <tanbomr@163.com>
2017-08-23 17:28:13 +0800

    图文识别调取php接口

--- src/main/java/com/fjs/cronus/api/OcrController.java
+++ src/main/java/com/fjs/cronus/api/OcrController.java
@@ -5,8 +5,15 @@ import com.fjs.cronus.service.client.TalosService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Administrator on 2017/8/17 0017.
@@ -15,8 +22,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/ocr/api")
 public class OcrController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private TalosService talosService;
+
+    @Value("${sale.url}")
+    private String saleUrl;
+
+    @Value("${sale.key}")
+    private String saleKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     /**
      * 调用第三方图文识别
@@ -30,6 +48,7 @@ public class OcrController {
     })
     @RequestMapping(value = "/v1/", method = RequestMethod.POST)//TODO 地址;
     public void ocrService(@RequestHeader(name = "Authorization") String token, @RequestBody ReqParamDTO reqParamDTO){
+        LOGGER.warn("OCR识别接口_OcrController_ocrService : reqParamDTO = " + ReflectionToStringBuilder.toString(reqParamDTO));
         talosService.ocrService(reqParamDTO, token);
     }
 
@@ -46,7 +65,31 @@ public class OcrController {
     })
     @RequestMapping(value = "/v1/saveIdCard", method = RequestMethod.POST)
     public void saveIdCard(@RequestBody IdCardDTO idCardDTO, @RequestHeader(name = "Authorization") String token){
-        //TODO 调用PHP接口;
+        String url = saleUrl + "addOrcInfo";
+        MultiValueMap<String,Object> param = new LinkedMultiValueMap<>();
+        param.add("key",saleKey);
+        param.add("customer_id",idCardDTO.getCustomer_id());
+        param.add("customer_name",idCardDTO.getCustomer_name());
+        param.add("customer_telephone",idCardDTO.getCustomer_telephone());
+        param.add("card_name",idCardDTO.getCard_name());
+        param.add("card_sex",idCardDTO.getCard_sex());
+        param.add("card_nation",idCardDTO.getCard_nation());
+        param.add("card_birth",idCardDTO.getCard_birth());
+        param.add("card_address",idCardDTO.getCard_address());
+        param.add("card_num",idCardDTO.getCard_num());
+        param.add("card_sign_org",idCardDTO.getCard_sign_org());
+        param.add("card_valid_start",idCardDTO.getCard_valid_start());
+        param.add("card_valid_end",idCardDTO.getCard_valid_end());
+        param.add("crm_attach_id",idCardDTO.getCrm_attach_id());
+        param.add("create_user_id",idCardDTO.getCreate_user_id());
+        param.add("create_user_name",idCardDTO.getCreate_user_name());
+        param.add("status","草稿");//未效验状态
+        param.add("update_user_id",idCardDTO.getCreate_user_id());
+        param.add("update_user_name",idCardDTO.getCreate_user_name());
+        param.add("type",idCardDTO.getType());
+        LOGGER.warn("保存身份证信息接口_OcrController_saveIdCard : idCardDTO = " + ReflectionToStringBuilder.toString(idCardDTO) + ", token = " + token);
+        String res = restTemplate.postForObject(url, param, String.class);
+        LOGGER.warn("保存身份证信息接口_OcrController_saveIdCard ：res = " + res);
     }
 
 
@@ -62,7 +105,27 @@ public class OcrController {
     })
     @RequestMapping(value = "/v1/saveDriverLicense", method = RequestMethod.POST)
     public void saveDriverLicense(@RequestBody DriverLicenseDTO driverLicenseDTO, @RequestHeader(name = "Authorization") String token){
-        //TODO 调用PHP接口;
+        String url = saleUrl + "addOrcInfo";
+        MultiValueMap<String,Object> param = new LinkedMultiValueMap<>();
+        param.add("key",saleKey);
+        param.add("customer_id",driverLicenseDTO.getCustomer_id());
+        param.add("customer_name",driverLicenseDTO.getCustomer_name());
+        param.add("customer_telephone",driverLicenseDTO.getCustomer_telephone());
+        param.add("driver_name",driverLicenseDTO.getDriver_name());
+        param.add("driver_num",driverLicenseDTO.getDriver_num());
+        param.add("driver_vehicle_type",driverLicenseDTO.getDriver_vehicle_type());
+        param.add("driver_start_date",driverLicenseDTO.getDriver_start_date());
+        param.add("driver_end_date",driverLicenseDTO.getDriver_end_date());
+        param.add("crm_attach_id",driverLicenseDTO.getCrm_attach_id());
+        param.add("create_user_id",driverLicenseDTO.getCreate_user_id());
+        param.add("create_user_name",driverLicenseDTO.getCreate_user_name());
+        param.add("status","草稿");
+        param.add("update_user_id",driverLicenseDTO.getCreate_user_id());
+        param.add("update_user_name",driverLicenseDTO.getCreate_user_name());
+        param.add("type",driverLicenseDTO.getType());
+        LOGGER.warn("保存驾驶证信息接口_OcrController_saveDriverLicense : driverLicenseDTO = " + ReflectionToStringBuilder.toString(driverLicenseDTO) + ", token = " + token);
+        String res = restTemplate.postForObject(url, param, String.class);
+        LOGGER.warn("保存驾驶证信息接口_OcrController_saveDriverLicense ：res = " + res);
     }
 
 
@@ -78,7 +141,28 @@ public class OcrController {
     })
     @RequestMapping(value = "/v1/saveDriverVehicle", method = RequestMethod.POST)
     public void saveDriverVehicle(@RequestBody DriverVehicleDTO driverVehicleDTO, @RequestHeader(name = "Authorization") String token){
-        //TODO 调用PHP接口;
+        String url = saleUrl + "addOrcInfo";
+        MultiValueMap<String,Object> param = new LinkedMultiValueMap<>();
+        param.add("key",saleKey);
+        param.add("customer_id",driverVehicleDTO.getCustomer_id());
+        param.add("customer_name",driverVehicleDTO.getCustomer_name());
+        param.add("customer_telephone",driverVehicleDTO.getCustomer_telephone());
+        param.add("driver_owner",driverVehicleDTO.getDriver_owner());
+        param.add("driver_plate_num",driverVehicleDTO.getDriver_plate_num());
+        param.add("driver_vehicle_type",driverVehicleDTO.getDriver_vehicle_type());
+        param.add("driver_vin",driverVehicleDTO.getDriver_vin());
+        param.add("driver_engine_num",driverVehicleDTO.getDriver_engine_num());
+        param.add("driver_register_date",driverVehicleDTO.getDriver_register_date());
+        param.add("crm_attach_id",driverVehicleDTO.getCrm_attach_id());
+        param.add("create_user_id",driverVehicleDTO.getCreate_user_id());
+        param.add("create_user_name",driverVehicleDTO.getCreate_user_name());
+        param.add("status","草稿");
+        param.add("update_user_id",driverVehicleDTO.getCreate_user_id());
+        param.add("update_user_name",driverVehicleDTO.getCreate_user_name());
+        param.add("type",driverVehicleDTO.getType());
+        LOGGER.warn("保存行驶证信息接口_OcrController_saveDriverVehicle : driverVehicleDTO = " + ReflectionToStringBuilder.toString(driverVehicleDTO) + ", token = " + token);
+        String res = restTemplate.postForObject(url, param, String.class);
+        LOGGER.warn("保存行驶证信息接口_OcrController_saveDriverVehicle ： res = " + res);
     }
 
 
@@ -94,7 +178,28 @@ public class OcrController {
     })
     @RequestMapping(value = "/v1/saveHouseholdRegister", method = RequestMethod.POST)
     public void saveHouseholdRegister(@RequestBody HouseholdRegisterDTO householdRegisterDTO, @RequestHeader(name = "Authorization") String token){
-        //TODO 调用PHP接口;
+        String url = saleUrl + "addOrcInfo";
+        MultiValueMap<String,Object> param = new LinkedMultiValueMap<>();
+        param.add("key",saleKey);
+        param.add("customer_id",householdRegisterDTO.getCustomer_id());
+        param.add("customer_name",householdRegisterDTO.getCustomer_name());
+        param.add("customer_telephone",householdRegisterDTO.getCustomer_telephone());
+        param.add("household_name",householdRegisterDTO.getHousehold_name());
+        param.add("household_sex",householdRegisterDTO.getHousehold_sex());
+        param.add("household_native_place",householdRegisterDTO.getHousehold_native_place());
+        param.add("household_birthday",householdRegisterDTO.getHousehold_birthday());
+        param.add("household_id_number",householdRegisterDTO.getHousehold_id_number());
+        param.add("household_people",householdRegisterDTO.getHousehold_people());
+        param.add("crm_attach_id",householdRegisterDTO.getCrm_attach_id());
+        param.add("create_user_id",householdRegisterDTO.getCreate_user_id());
+        param.add("create_user_name",householdRegisterDTO.getCreate_user_name());
+        param.add("status","草稿");
+        param.add("update_user_id",householdRegisterDTO.getCreate_user_id());
+        param.add("update_user_name",householdRegisterDTO.getCreate_user_name());
+        param.add("type",householdRegisterDTO.getType());
+        LOGGER.warn("保存户口本信息接口_OcrController_saveHouseholdRegister : householdRegisterDTO = " + ReflectionToStringBuilder.toString(householdRegisterDTO) + ", token = " + token);
+        String res = restTemplate.postForObject(url, param, String.class);
+        LOGGER.warn("保存户口本信息接口_OcrController_saveHouseholdRegister : res = " + res);
     }
 
 
--- src/main/java/com/fjs/cronus/dto/ocr/OcrCronusBaseDTO.java
+++ src/main/java/com/fjs/cronus/dto/ocr/OcrCronusBaseDTO.java
@@ -14,10 +14,17 @@ public class OcrCronusBaseDTO {
 
     private Long create_user_id;// int(11) DEFAULT '0' COMMENT '创建人ID',
     private String create_user_name;// varchar(50) DEFAULT 'OCR' COMMENT '创建人名称',
-    private Long create_time;
     private Long update_user_id;// int(11) NOT NULL COMMENT '更新人ID',
     private String update_user_name;// varchar(50) DEFAULT NULL COMMENT '更新人名称',
-    private Long update_time;
+    private String type;// 证件类型;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     public Long getCustomer_id() {
         return customer_id;
@@ -67,14 +74,6 @@ public class OcrCronusBaseDTO {
         this.create_user_name = create_user_name;
     }
 
-    public Long getCreate_time() {
-        return create_time;
-    }
-
-    public void setCreate_time(Long create_time) {
-        this.create_time = create_time;
-    }
-
     public Long getUpdate_user_id() {
         return update_user_id;
     }
@@ -91,11 +90,4 @@ public class OcrCronusBaseDTO {
         this.update_user_name = update_user_name;
     }
 
-    public Long getUpdate_time() {
-        return update_time;
-    }
-
-    public void setUpdate_time(Long update_time) {
-        this.update_time = update_time;
-    }
 }
