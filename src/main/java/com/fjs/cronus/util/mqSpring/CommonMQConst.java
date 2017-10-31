6c1302c4135decef55498b0dfb8a45d7c09dbce8
zhanglei <zhanglei@fangjinsuo.com>
2017-10-31 13:21:51 +0800

    add 增加客户MQtag

--- src/main/java/com/fjs/cronus/util/mqSpring/CommonMQConst.java
+++ src/main/java/com/fjs/cronus/util/mqSpring/CommonMQConst.java
@@ -7,5 +7,8 @@ public class CommonMQConst {
 
     public static final String THEA_HXF_01 = "THEA_HXF_01";//晓峰测试数据
 
+    public static final String CRIUS_SERVICE_CUSTOMER = "ServiceCustomer";//服务合同更新客户状态
+
+    public static final String CRIUS_CONTRACT_CUSTOMER = "ContractCustomer";//放款合同更新客户状态
 
 }
