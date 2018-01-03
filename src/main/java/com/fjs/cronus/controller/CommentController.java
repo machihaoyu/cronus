4d8d720a1dddd69e92f5dca63f01cad78dae44a5
zhanglei <zhanglei@fangjinsuo.com>
2018-01-03 17:02:09 +0800

    modify

--- src/main/java/com/fjs/cronus/controller/CommentController.java
+++ src/main/java/com/fjs/cronus/controller/CommentController.java
@@ -100,7 +100,7 @@ public class CommentController {
                     boolean flag = false;
                     for (String str : listRole) {
                         RoleDTO roleDTO = thorUcService.getRoleInfo(token, "name",str,Integer.valueOf(userInfoDTO.getCompany_id()));
-                        if (!"团队长".equals(roleDTO.getName())){
+                        if ("团队长".equals(roleDTO.getName())){
                             flag = true;
                         }
                     }
