a67236cb337761546b9facc4ae58f41a5590b118
zhanglei <zhanglei@fangjinsuo.com>
2018-01-05 18:06:00 +0800

    fix

--- src/main/java/com/fjs/cronus/Common/ResultResource.java
+++ src/main/java/com/fjs/cronus/Common/ResultResource.java
@@ -4,7 +4,7 @@ package com.fjs.cronus.Common;
  * Created by msi on 2017/9/14.
  */
 public class ResultResource {
-
+    public static int CODE_SUCCESS = 0;
     public static String MESSAGE_SUCCESS = "成功";
     // 其他错误
     public static int CODE_ERROR = 0;
