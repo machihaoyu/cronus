5d4d2a54d65bde774d93badb7c563ca4c91a126a
weiwenyu <weiwenyu@fangjinsuo.com>
2018-01-31 15:51:14 +0800

    短信接口

--- src/main/java/com/fjs/cronus/service/SmsService.java
+++ src/main/java/com/fjs/cronus/service/SmsService.java
@@ -1,11 +1,11 @@
 package com.fjs.cronus.service;
 
+import com.fjs.cronus.model.SysConfig;
 import com.fjs.cronus.util.SmsUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  */
@@ -15,6 +15,9 @@ public class SmsService {
     @Resource
     SmsUtils smsUtils;
 
+    @Autowired
+    private SysConfigService sysConfigService;
+
     /**
      * 自动分配发送短信
      *
@@ -23,22 +26,34 @@ public class SmsService {
      * @return
      */
     public String sendSmsForAutoAllocate(String sendPhone, String customerName) {
-        String smsContent = "[测试数据]房金所为您分配了客户名："+customerName+"，请注意跟进。";
-        String smsResult = smsUtils.sendBatchMessage(sendPhone, smsContent);
+        String smsResult = "";
+        SysConfig sysConfig = sysConfigService.getConfigByName("sysUse");
+        if (sysConfig.getConValue().equals("1")) {
+            String smsContent = "【房金云】房金所为您分配了客户名：" + customerName + "，请注意跟进。";
+            smsResult = smsUtils.sendBatchMessage(sendPhone, smsContent);
+        }
         return smsResult;
     }
 
     public String sendNonCommunicate(String customerName,String telephonenumber)
     {
-        String smsContent = "系统将新客户分配给了你,姓名:"+customerName+",请注意跟进(15分钟内未添加沟通日志，客户自动划走)";
-        String smsResult = "";//smsUtils.sendBatchMessage(telephonenumber, smsContent);
+        String smsResult = "";
+        SysConfig sysConfig = sysConfigService.getConfigByName("sysUse");
+        if (sysConfig.getConValue().equals("1")) {
+            String smsContent = "【房金云】系统将新客户分配给了你,姓名:" + customerName + ",请注意跟进(15分钟内未添加沟通日志，客户自动划走)。";
+            smsResult = smsUtils.sendBatchMessage(telephonenumber, smsContent);
+        }
         return smsResult;
     }
 
-    public String sendCRMAssistant(String telephonenumber)
+    public String sendCRMAssistant(String telephoneNumber)
     {
-        String smsContent = "自动分配名额已满，请及时增加分配名额。";
-        String smsResult = smsUtils.sendBatchMessage(telephonenumber, smsContent);
+        String smsResult = "";
+        SysConfig sysConfig = sysConfigService.getConfigByName("sysUse");
+        if (sysConfig.getConValue().equals("1")) {
+            String smsContent = "【房金云】自动分配名额已满，请及时增加分配名额。";
+            smsResult = smsUtils.sendBatchMessage(telephoneNumber, smsContent);
+        }
         return smsResult;
     }
 
