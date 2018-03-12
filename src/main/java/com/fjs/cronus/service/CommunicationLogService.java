5f123d603a17b4394d543dcbe9a26efcae51274c
weiwenyu <weiwenyu@fangjinsuo.com>
2018-03-12 17:40:52 +0800

    暂未接通，短信发送内容更新

--- src/main/java/com/fjs/cronus/service/CommunicationLogService.java
+++ src/main/java/com/fjs/cronus/service/CommunicationLogService.java
@@ -118,8 +118,8 @@ public class CommunicationLogService {
                         break;
                     case not_yet_connected:
                         try {
-                            String content = "尊敬的客户，您的申请已受理，因未能与您取得联系，如需资金，可联系专属顾问" +
-                                    userInfoDTO.getName() + ": " + userInfoDTO.getTelephone() + ",更多资讯请关注官方微信：房金所";
+                            String content = "尊敬的客户，您的申请已受理，因联系不上，如需融资可联系" +
+                                    userInfoDTO.getName() + ": " + userInfoDTO.getTelephone() + "，更多融资机会请关注官方微信：房金所";
                             smsService.sendCommunication(customerphone, content);
                         } catch (Exception e) {
                             e.printStackTrace();
