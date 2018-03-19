32f0e636639580019e220502ab2c662d26c52fa9
weiwenyu <weiwenyu@fangjinsuo.com>
2018-03-19 15:32:54 +0800

    not_yet_connected

--- src/main/java/com/fjs/cronus/service/CommunicationLogService.java
+++ src/main/java/com/fjs/cronus/service/CommunicationLogService.java
@@ -127,6 +127,7 @@ public class CommunicationLogService {
                         try {
                             String content = "尊敬的客户，您的申请已受理，因联系不上，如需融资可联系" +
                                     userInfoDTO.getName() + ": " + userInfoDTO.getTelephone() + "，更多融资机会请关注官方微信：房金所";
+                            stringBuilder.append(content);
                             smsService.sendCommunication(customerphone, content);
                         } catch (Exception e) {
                             e.printStackTrace();
