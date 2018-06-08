aca501c70361fdb80a74ef3d096f8f907e6f9a7e
lihong <lihong@fangjinsuo.com>
2018-06-08 16:08:39 +0800

    修改日志文字

--- src/main/java/com/fjs/cronus/util/SingleCutomerAllocateDevInfoUtil.java
+++ src/main/java/com/fjs/cronus/util/SingleCutomerAllocateDevInfoUtil.java
@@ -33,33 +33,33 @@ public class SingleCutomerAllocateDevInfoUtil {
     public static final String k20 = "redis分布式锁超时";
 
     // ------------------ 商机找业务员规则 start ---------------------------------
-    public static final String k21 = "根据城市、媒体查一级吧queue大小";
-    public static final String k22 = "循环城市、媒体queue找一级吧,";
-    public static final String k23 = "获取某一级吧、某媒体、某月的已分配数,";
+    public static final String k21 = "找一级吧queue大小";
+    public static final String k22 = "找一级吧,";
+    public static final String k23 = "找一级吧已分配数,";
     public static final String k24 = "请求商机系统,";
     public static final String k25 = "获取关注的媒体,";
-    public static final String k46 = "[特殊分配队列]进入特殊分配队列分支，找业务员,";
-    public static final String k26 = "根据某一级吧、某媒体（特殊队列）、某月找业务员queue大小,";
-    public static final String k27 = "[特殊分配队列]循环某一级吧、某媒体（特殊队列）、某月queue找业务员,";
-    public static final String k28 = "[特殊分配队列]根据一级吧、业务员、媒体（特殊队列）、月找对应分配数,";
-    public static final String k29 = "[特殊分配队列]根据一级吧、业务员、媒体（特殊队列）、月找总队列对应分配数,";
-    public static final String k30 = "[特殊分配队列]从queue找到业务员,";
-    public static final String k31 = "[总分配队列]进入总分配队列分支，找业务员,";
-    public static final String k32 = "[总分配队列]获取城市、总队列、月queue大小,";
-    public static final String k33 = "[总分配队列]循环城市、总队列、月queue找业务员,";
-    public static final String k34 = "[总分配队列]根据城市、总队列、月queue中找的业务员,找分配数,";
-    public static final String k35 = "[总分配队列]进入在总队列中，判断特殊队列数据（有特殊队列，但未找到业务员）情况，";
-    public static final String k36 = "[总分配队列]进入在总队列中，判断特殊队列数据（有特殊队列，但未找到业务员）情况，找分配数，";
+    public static final String k46 = "[特殊分配队列]进入特殊分配队列分支,";
+    public static final String k26 = "[特殊分配队列]找业务员queue大小,";
+    public static final String k27 = "[特殊分配队列]从queue找业务员,";
+    public static final String k28 = "[特殊分配队列]找分配数,";
+    public static final String k29 = "[特殊分配队列]找总队列分配数,";
+    public static final String k30 = "[特殊分配队列]找到业务员,";
+    public static final String k31 = "[总分配队列]进入总分配队列分支,";
+    public static final String k32 = "[总分配队列]找业务员queue大小,";
+    public static final String k33 = "[总分配队列]从queue找业务员,";
+    public static final String k34 = "[总分配队列]找分配数,";
+    public static final String k35 = "[总分配队列]进入判断特殊队列数据分支，";
+    public static final String k36 = "[总分配队列]找分配数，";
     public static final String k38 = "[总分配队列]从queue找到业务员,";
     public static final String k39 = "通过商机规则，找到业务员,";
     // ------------------ 商机找业务员规则 end ---------------------------------
 
     // ------------------ 老顾客，借用商机的总队列找业务员 start ---------------------------------
-    public static final String k40 = "老用户根据城市、媒体获取一级吧queue大小,";
-    public static final String k41 = "老用户循环城市、媒体的一级吧queue找一级吧,";
-    public static final String k42 = "老用户获取业务员总队列queue大小,";
-    public static final String k43 = "老用户循环queue,未找业务员,";
-    public static final String k44 = "老用户循环queue,找到业务员,";
+    public static final String k40 = "老用户找一级吧queue大小,";
+    public static final String k41 = "老用户从queue找一级吧,";
+    public static final String k42 = "老用户找业务员总队列queue大小,";
+    public static final String k43 = "老用户 未找业务员,";
+    public static final String k44 = "老用户 找到业务员,";
     // ------------------ 老顾客，借用商机的总队列找业务员 end ---------------------------------
 
     public static final String k45 = "ocdcservice.addOcdcCustomer分配失败";
