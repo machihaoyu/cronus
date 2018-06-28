ed96e042d66bcc90a9eda0ceb1fb443596d6eb03
lihong <lihong@fangjinsuo.com>
2018-06-28 11:00:14 +0800

    分配调试bug

--- src/main/java/com/fjs/cronus/service/allocatecustomer/v2/AutoAllocateServiceV2.java
+++ src/main/java/com/fjs/cronus/service/allocatecustomer/v2/AutoAllocateServiceV2.java
@@ -1388,7 +1388,7 @@ public class AutoAllocateServiceV2 {
         );
 
         // 获取业务员角色列表
-        ThorApiDTO<JSONObject> thorApiDTO = thorService.findRolesBySalesmanidAndCompanyid(getwayToken, salesmanId, companyid);
+        ThorApiDTO<Object> thorApiDTO = thorService.findRolesBySalesmanidAndCompanyid(getwayToken, salesmanId, companyid);
 
         SingleCutomerAllocateDevInfoUtil.local.get().setInfo4Rep(SingleCutomerAllocateDevInfoUtil.k60 + "调试"
                 , ImmutableMap.of("thorApiDTO", thorApiDTO)
@@ -1405,8 +1405,12 @@ public class AutoAllocateServiceV2 {
             throw new CronusException(CronusException.Type.CRM_PARAMS_ERROR, "获取用户角色列表异常,接口result非0，message=" + thorApiDTO.getMessage());
         }
 
+        SingleCutomerAllocateDevInfoUtil.local.get().setInfo4Rep(SingleCutomerAllocateDevInfoUtil.k60 + "调试"
+                , ImmutableMap.of("data", thorApiDTO.getData())
+        );
+
         // 角色列表（无数据 or null 视为无此角色）
-        JSONObject data = thorApiDTO.getData();
+        JSONObject data = null;
         if (data == null) {
             SingleCutomerAllocateDevInfoUtil.local.get().setInfo4Rep(SingleCutomerAllocateDevInfoUtil.k60 + "获取角色列表"
                     , ImmutableMap.of("接口返回data", "data为null")
